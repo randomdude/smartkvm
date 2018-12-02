713d3a73ac09665b8dc10c704f443deb160fc175
aliz <aliz>
2018-12-02 07:49:17 +0000

    fix file redirection

--- genkeys.sh
+++ genkeys.sh
@@ -1,1 +1,1 @@
-grep define /usr/include/linux/input-event-codes.h | grep KEY_ | awk '{ print "KEYS[" $3 "] = \"" $2 "\";" }'
+grep define /usr/include/linux/input-event-codes.h | grep KEY_ | awk '{ print "KEYS[" $3 "] = \"" $2 "\";" }' > keys.h
