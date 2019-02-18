0004b6fb33b90772f7dbaeebc3bed34b2dea47d4
aliz <aliz>
2019-02-18 19:30:01 +0800

    update genkeys.sh

--- genkeys.sh
+++ genkeys.sh
@@ -1,1 +1,10 @@
-grep define /usr/include/linux/input-event-codes.h | grep KEY_ | awk '{ print "KEYS[" $3 "] = \"" $2 "\";" }' > keys-linux.h
+
+echo > keys-linux.h
+
+echo struct keyNameAndCode linuxKeys[] = >> keys-linux.h
+echo { >> keys-linux.h
+
+grep define /usr/include/linux/input-event-codes.h | egrep "^#define KEY_[A-Z0-9_]*\s*(0x)?[0-9a-fA-F]*(\s*/\*.*\*/(\w*?))?$" | awk '{ print "\t{ .name = \"" $2 "\", .code = " $3 " }," }' >> keys-linux.h
+
+echo -e \\t{ .name = NULL, .code = 0 } >> keys-linux.h
+echo }\; >> keys-linux.h
