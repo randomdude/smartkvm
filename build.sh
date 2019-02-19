772ef3259099cd45b42f83d301de07b407ac4f4b
aliz <aliz>
2019-02-19 23:09:01 +0800

    Add arduino stuff to build script

--- build.sh
+++ build.sh
@@ -1,15 +1,19 @@
+
+# Install the Arduino and Teensy tools
 wget https://downloads.arduino.cc/arduino-1.8.8-linux64.tar.xz
 tar -Jxf arduino-1.8.8-linux64.tar.xz
 ./arduino-1.8.8/install.sh >/dev/null
-
 wget https://www.pjrc.com/teensy/td_145/TeensyduinoInstall.linux64
 chmod +x TeensyduinoInstall.linux64
 ./TeensyduinoInstall.linux64 --dir=`pwd`/arduino-1.8.8
 
+# Now we can build the host-side C stuff
 ./genkeys.sh
 ./genkeys-teensy.sh `pwd`/aruino-1.8.8
-
 gcc host.c -o host
 chmod +x ./host
 
-tar -czvf - host
+# And then the teensy code.
+./arduino-1.8.8/arduino --board teens:avr:teensy2:speed=16,usb=hid,keys=en-gb -v -pref build.path=usbext-build
+
+ls host usbext-build/usbext.ino.hex
--- usbext/usbext.ino
+++ usbext/usbext.ino
@@ -1,4 +1,4 @@
-#include "keyboard.h"
+#include "Keyboard.h"
 
 #define HWSERIAL Serial1
 
